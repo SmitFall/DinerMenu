63ed777a7df98a93926a7451449f96d28edfc219
Smitfall <captainfalconrsmith@gmail.com>
2022-03-29 08:57:06 -0600

    added header and soup display

--- DinerMenuForm/DinerMenuForm.Designer.vb
+++ DinerMenuForm/DinerMenuForm.Designer.vb
@@ -22,10 +22,90 @@ Partial Class DinerMenuForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FIshButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(12, 524)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(152, 93)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "S&oup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(170, 524)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(152, 93)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "S&alad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FIshButton
+        '
+        Me.FIshButton.Location = New System.Drawing.Point(328, 524)
+        Me.FIshButton.Name = "FIshButton"
+        Me.FIshButton.Size = New System.Drawing.Size(152, 93)
+        Me.FIshButton.TabIndex = 2
+        Me.FIshButton.Text = "&Fish"
+        Me.FIshButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.Location = New System.Drawing.Point(902, 524)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(152, 93)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.Font = New System.Drawing.Font("MV Boli", 40.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(216, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(639, 104)
+        Me.DinerNameLabel.TabIndex = 4
+        Me.DinerNameLabel.Text = "The Maple Diner"
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("MV Boli", 13.8!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(17, 125)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(1025, 379)
+        Me.DisplaySpecialLabel.TabIndex = 5
+        Me.DisplaySpecialLabel.Text = "Label1"
+        '
+        'DinerMenuForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaption
+        Me.ClientSize = New System.Drawing.Size(1066, 629)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FIshButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Name = "DinerMenuForm"
+        Me.Text = "The Maple Diner"
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FIshButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents DisplaySpecialLabel As Label
 End Class
--- DinerMenuForm/DinerMenuForm.vb
+++ DinerMenuForm/DinerMenuForm.vb
@@ -1,3 +1,30 @@
-﻿Public Class DinerMenuForm
+﻿'Fallon Smith
+'RCEt0265
+'Spring 2022
+'Diner Menu Program
+'
 
+Public Class DinerMenuForm
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        Me.Close()
+    End Sub
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        DisplaySpecialLabel.Text = "Tuscan Potato Soup  $12.50 " & vbNewLine _
+            & "Made from locally grown potatoes. It has some of the best hand picked ingredients. " & vbNewLine _
+            & "It has a great source of protein and vegatebles." & vbNewLine _
+            & "Rumors say some of the mystery meat in the soup is made out of people who tried to not pay for the meal"
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+
+    End Sub
+
+    Private Sub FIshButton_Click(sender As Object, e As EventArgs) Handles FIshButton.Click
+
+    End Sub
+
+    Private Sub DisplaySpecialLabel_Click(sender As Object, e As EventArgs) Handles DisplaySpecialLabel.Click
+
+    End Sub
 End Class
